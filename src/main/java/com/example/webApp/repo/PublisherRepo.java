fce85491defccbe27cafb8b0d26708ded404c5ba
Ricky.You <1350753448@qq.com>
2023-01-18 09:58:50 +0000

    Remove not necessary import and public keyword

--- src/main/java/com/example/webApp/repo/PublisherRepo.java
+++ src/main/java/com/example/webApp/repo/PublisherRepo.java
@@ -1,9 +1,8 @@
 package com.example.webApp.repo;
 
 import com.example.webApp.domain.Publisher;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublisherRepo extends JpaRepository<Publisher, Long> {
-    public Publisher findByName(String name);
+    Publisher findByName(String name);
 }
